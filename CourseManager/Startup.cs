f73e6e43672d741865d8bc87c7bcba0228549ba8
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-08 18:40:22 -0700

    Change App.xaml settings to start the app from Startup

--- CourseManager/Startup.cs
+++ CourseManager/Startup.cs
@@ -20,5 +20,6 @@ namespace CourseManager
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<MainViewModel>();
+        }
     }
 }
