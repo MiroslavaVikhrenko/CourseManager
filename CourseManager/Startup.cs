cc63974845e15ad3fcd0c70b662912a4c23f9a07
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-08 18:33:22 -0700

    Create a folder structure and a Startup class

---
+++ CourseManager/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using CourseManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CourseManager
+{
+    //this where app gets started, hooks for Caliburn
+    internal class Startup : BootstrapperBase //using Caliburn.Micro
+    {
+        public Startup()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainViewModel>();
+    }
+}
