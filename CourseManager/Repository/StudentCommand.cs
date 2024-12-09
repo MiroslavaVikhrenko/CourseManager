41fc3d0381cf55f21faa2bc944e46b105941b238
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-08 19:04:03 -0700

    Create a StudentCommand using dapper and sqlclient

---
+++ CourseManager/Repository/StudentCommand.cs
@@ -0,0 +1,35 @@
+﻿using CourseManager.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Repository
+{
+    internal class StudentCommand
+    {
+        private string _connectionString;
+
+        public StudentCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IList<StudentModel> GetList()
+        {
+            List<StudentModel> students = new List<StudentModel>();
+
+            var sql = "Student_GetList"; //stored procedure
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                students = connection.Query<StudentModel>(sql).ToList(); //using Dapper
+            }
+
+            return students;
+        }
+    }
+}
